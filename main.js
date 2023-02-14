fdeab90234fff233b0d39081cc93f84c10ddd105
Gloria Reina <gloria.l.reina11@gmail.com>
2023-02-14 15:33:00 -0500

    loop exercise done, way to go!

---
+++ main.js
@@ -0,0 +1,27 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for(var i = 0; i< cohortOneStudents.length; i++){
+    // console.log(cohortOneStudents[i])
+}
+
+let gloriasHobbies= ["exercise","eating", "dancing", "hicking", "reading", "learning","hanging out"]
+
+ for(let i = 0; i < gloriasHobbies.length; i++ ){
+
+        if(gloriasHobbies[i] === "dancing" ){
+             console.log(`My absolute favorite interest is: ${gloriasHobbies[i]}`)
+        }
+        else {
+             console.log( `One of my interests is: ${gloriasHobbies[i]}`)
+        }
+ }
+
+ //2nd option:
+ /*for( var hobbies= 0; hobbies< gloriasHobbies.length; hobbies++){
+    console.log( `One of my interests is:${gloriasHobbies[hobbies]}`)
+     if(gloriasHobbies[hobbies]=== gloriasHobbies[2]){
+            console.log(`My absolute favorite interest is: ${gloriasHobbies[hobbies]}`)}
+        }*/
+
+    
+    
